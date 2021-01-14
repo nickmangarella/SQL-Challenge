d32d41e20706a5b901bbbf9690024dc2e4c55a8a
nickm <nickmangarella89@gmail.com>
2021-01-13 21:57:47 -0500

    added erd png and uploaded tables

--- EmployeeSQL/employee_database.sql
+++ EmployeeSQL/employee_database.sql
@@ -1,40 +1,74 @@
--- Create departments table and view column datatypes
-CREATE TABLE departments(
-	dept_no string
-	dept_name VARCHAR
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR(50)   NOT NULL,
+    "dept_name" VARCHAR(50)   ,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
 );
 
--- Create dept_emp table and view column datatypes
-CREATE TABLE dept_emp(
-	emp_no int
-	dept_no string
+CREATE TABLE "dept_emp" (
+    "emp_no" INT   NOT NULL,
+    "dept_no" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
 );
 
--- Create dept_manager table and view column datatypes
-CREATE TABLE dept_manager(
-	dept_no string
-	emp_no int
+CREATE TABLE "dept_manager" (
+    "dept_no" VARCHAR(50)   NOT NULL,
+    "emp_no" INT   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "dept_no","emp_no"
+     )
 );
 
--- Create employees table and view column datatypes
-CREATE TABLE employees(
-	emp_no int
-	emp_title_id string
-	birth_date
-	first_name VARCHAR
-	last_name VARCHAR
-	sex 
-	hire_date 
+CREATE TABLE "employees" (
+    "emp_no" INT   NOT NULL,
+    "emp_title_id" VARCHAR(50)   ,
+    "birth_date" DATE   ,
+    "first_name" VARCHAR(50)   ,
+    "last_name" VARCHAR(50)   ,
+    "sex" VARCHAR(50)   ,
+    "hire_date" DATE   ,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
 );
 
--- Create salaries table and view column datatypes
-CREATE TABLE salaries(
-	emp_no int
-	salary int
+CREATE TABLE "salaries" (
+    "emp_no" INT   NOT NULL,
+    "salary" INT   ,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "emp_no"
+     )
 );
 
--- Create titles table and view column datatypes
-CREATE TABLE titles(
-	title_id string
-	title VARCHAR
+CREATE TABLE "titles" (
+    "title_id" VARCHAR(50)   NOT NULL,
+    "title" VARCHAR(50)   ,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
 );
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("title_id");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
