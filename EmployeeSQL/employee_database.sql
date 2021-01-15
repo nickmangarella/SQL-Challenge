0aeb09aa2187315fe2a93ebe9c87bb78510c8abd
nickm <nickmangarella89@gmail.com>
2021-01-15 18:14:32 -0500

    finished queries

--- EmployeeSQL/employee_database.sql
+++ EmployeeSQL/employee_database.sql
@@ -106,4 +106,30 @@ FROM employees e
 JOIN dept_emp de ON e.emp_no = de.emp_no
 JOIN departments d ON de.dept_no = d.dept_no;
 
-SELECT 
+SELECT first_name,
+	last_name,
+	sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+SELECT e.emp_no,
+	e.last_name,
+	e.first_name,
+	d.dept_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales';
+
+SELECT e.emp_no,
+	e.last_name,
+	e.first_name,
+	d.dept_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development';
+
+SELECT last_name, COUNT(*)
+FROM employees
+GROUP BY last_name;
